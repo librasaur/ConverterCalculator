6f2c3dc02e9e62ea67d15e787527d0715df22c30
Lizzie <lizzie.briggs10@gmail.com>
2019-10-25 17:10:17 +0100

    Started project
    Created project solution and added basic code to display window

---
+++ ConverterCalculator/ConverterCalculator/Calculator.cpp
@@ -0,0 +1,15 @@
+#include "Calculator.h"
+
+using namespace ConverterCalculator;
+using namespace System;
+using namespace System::Windows::Forms;
+
+int main(array<System::String ^> ^args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Application::Run(gcnew Calculator());
+
+	return 0;
+}
